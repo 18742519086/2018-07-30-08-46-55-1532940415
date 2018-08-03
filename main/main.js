3c5fa41a61987e852c9adb01045413f0856ff03f
daibowen <972771202@qq.com>
2018-08-03 11:35:39 +0800

    update main.js

--- main/main.js
+++ main/main.js
@@ -1,3 +1,12 @@
-module.exports = function main() {
-  // Write your code here
+module.exports = function main(input) {
+    // Write your code here
+	var a;
+	var sum = 0;
+	var b = input;
+	for(var i = 0; i < input.length; i++){
+		a = b % 10; 
+		sum = sum + a;
+		b = (b - a) / 10;
+	}
+	return sum;
 };
